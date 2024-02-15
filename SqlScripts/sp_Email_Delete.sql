1d70dea7f1311676b3d641e25537d231a681e374
Hentzware <hentzware@gmail.com>
2024-02-15 14:10:52 +0100

    Updated Email SPs

--- SqlScripts/sp_Email_Delete.sql
+++ SqlScripts/sp_Email_Delete.sql
@@ -1,4 +1,4 @@
 CREATE DEFINER=`root`@`%` PROCEDURE `sp_Email_Delete`(IN in_id varchar(36))
 BEGIN
-    UPDATE Email SET IsDeleted = true WHERE Id = in_id;
+    UPDATE Emails SET IsDeleted = true WHERE Id = in_id;
 END
--- SqlScripts/sp_Email_GetByEmail.sql
+++ SqlScripts/sp_Email_GetByEmail.sql
@@ -1,4 +1,4 @@
 CREATE DEFINER=`root`@`%` PROCEDURE `sp_Email_GetByEmail`(in in_email varchar(36))
 BEGIN
-    SELECT * FROM Email WHERE Email = in_email;
+    SELECT * FROM Emails WHERE Email = in_email;
 END
