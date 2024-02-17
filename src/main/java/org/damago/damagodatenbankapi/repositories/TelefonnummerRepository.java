78aefcbd8eac6985cd43d8d6cce639c438ebe092
Henning Entz <entz.henning@gmail.com>
2024-02-18 00:21:04 +0100

    DamagoDatenbank-API-2 Telefonnummer Repository erstellen

---
+++ src/main/java/org/damago/damagodatenbankapi/repositories/TelefonnummerRepository.java
@@ -0,0 +1,36 @@
+package org.damago.damagodatenbankapi.repositories;
+
+import org.damago.damagodatenbankapi.entities.Telefonnummer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.query.Procedure;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional(readOnly = true)
+public interface TelefonnummerRepository extends JpaRepository<Telefonnummer, String> {
+    @Procedure
+    String sp_Phones_Add(String telefonnummer);
+
+    @Procedure
+    void sp_Phones_Delete(String id);
+
+    @Procedure
+    void sp_Phones_DeletePermanent(String id);
+
+    @Procedure
+    Iterable<Telefonnummer> sp_Phones_Get();
+
+    @Procedure
+    Telefonnummer sp_Phones_GetById(String id);
+
+    @Procedure
+    Iterable<Telefonnummer> sp_Phones_GetDeleted();
+
+    @Procedure
+    Iterable<Telefonnummer> sp_Phones_Search(String telefonnummer);
+
+    @Procedure
+    void sp_Phones_Undelete(String id);
+
+    @Procedure
+    Telefonnummer sp_Phones_Update(String id, String telefonnummer);
+}
