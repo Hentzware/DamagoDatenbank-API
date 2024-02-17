8a0734aec3576d94a01e755a381bed84bd863181
Henning Entz <entz.henning@gmail.com>
2024-02-18 00:47:53 +0100

    DamagoDatenbank-API-1 Telefonnummer Service erstellen

---
+++ src/main/java/org/damago/damagodatenbankapi/requests/telefonnummer/EditTelefonnummerRequest.java
@@ -0,0 +1,11 @@
+package org.damago.damagodatenbankapi.requests.telefonnummer;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class EditTelefonnummerRequest {
+    private String id;
+    private String telefonnummer;
+}
---
+++ src/main/java/org/damago/damagodatenbankapi/requests/telefonnummer/GetTelefonnummerRequest.java
@@ -0,0 +1,10 @@
+package org.damago.damagodatenbankapi.requests.telefonnummer;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class GetTelefonnummerRequest {
+    private String id;
+}
