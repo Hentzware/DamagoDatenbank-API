83121e55f74044bdc304211fbbaad637f3751e97
Henning Entz <entz.henning@gmail.com>
2024-02-18 15:22:56 +0100

    RaumService hinzugefügt.

--- src/main/java/org/damago/damagodatenbankapi/repositories/RaumRepository.java
+++ src/main/java/org/damago/damagodatenbankapi/repositories/RaumRepository.java
@@ -8,29 +8,29 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public interface RaumRepository extends JpaRepository<Raum, String> {
     @Procedure
-    String sp_Room_Add(String name, String nr);
+    String sp_Rooms_Add(String name, String nr);
 
     @Procedure
-    void sp_Room_Delete(String id);
+    void sp_Rooms_Delete(String id);
 
     @Procedure
-    void sp_Room_DeletePermanent(String id);
+    void sp_Rooms_DeletePermanent(String id);
 
     @Procedure
-    Iterable<Raum> sp_Room_Get();
+    Iterable<Raum> sp_Rooms_Get();
 
     @Procedure
-    Raum sp_Room_GetById(String id);
+    Raum sp_Rooms_GetById(String id);
 
     @Procedure
-    Iterable<Raum> sp_Room_GetDeleted();
+    Iterable<Raum> sp_Rooms_GetDeleted();
 
     @Procedure
-    Iterable<Raum> sp_Room_Search(String name, String nr);
+    Iterable<Raum> sp_Rooms_Search(String name, String nr);
 
     @Procedure
-    void sp_Room_Undelete(String id);
+    void sp_Rooms_Undelete(String id);
 
     @Procedure
-    void sp_Room_Update(String id, String name, String nr);
+    void sp_Rooms_Update(String id, String name, String nr);
 }
