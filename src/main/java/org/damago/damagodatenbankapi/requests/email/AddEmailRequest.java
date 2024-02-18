b6f1b249de78f1f46e7d74d713b08ad3e1538280
Henning Entz <entz.henning@gmail.com>
2024-02-18 12:31:38 +0100

    Email Requests und Response hinzugefügt.

---
+++ src/main/java/org/damago/damagodatenbankapi/requests/email/AddEmailRequest.java
@@ -0,0 +1,10 @@
+package org.damago.damagodatenbankapi.requests.email;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AddEmailRequest {
+    private String email;
+}
