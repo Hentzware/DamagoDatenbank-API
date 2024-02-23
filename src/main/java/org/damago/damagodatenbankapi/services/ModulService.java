c6b74462c4d812d33c0840ef0ac1d35be013b6cd
Rojhat Erdogan <Ro.Erdogan@icloud.com>
2024-02-23 19:45:45 +0100

    Module - noch nicht Fertig - sp-mysql muss ich noch einpflegen

---
+++ src/main/java/org/damago/damagodatenbankapi/services/ModulService.java
@@ -0,0 +1,21 @@
+package org.damago.damagodatenbankapi.services;
+
+import org.damago.damagodatenbankapi.requests.inventar.*;
+import org.damago.damagodatenbankapi.requests.modul.*;
+import org.damago.damagodatenbankapi.responses.InventarResponse;
+import org.damago.damagodatenbankapi.responses.ModulResponse;
+
+public interface ModulService {
+    ModulResponse Add(AddModulRequest request);
+
+    void Delete(DeleteModulRequest request, boolean permanent);
+
+    ModulResponse Edit(EditModulRequest request);
+
+    Iterable<ModulResponse> Get(boolean deleted);
+
+    ModulResponse GetById(GetModulRequest request);
+
+    Iterable<ModulResponse> Search(SearchModulRequest request);
+}
+
---
+++ src/main/java/org/damago/damagodatenbankapi/services/ModulServiceImpl.java
@@ -0,0 +1,70 @@
+package org.damago.damagodatenbankapi.services;
+
+import org.damago.damagodatenbankapi.entities.Modul;
+import org.damago.damagodatenbankapi.repositories.ModulRepository;
+import org.damago.damagodatenbankapi.requests.modul.*;
+import org.damago.damagodatenbankapi.responses.ModulResponse;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ModulServiceImpl implements ModulService {
+    private final ModulRepository modulRepository;
+    private final ModelMapper modelMapper;
+
+    public ModulServiceImpl(ModulRepository modulRepository, ModelMapper modelMapper) {
+        this.modulRepository = modulRepository;
+        this.modelMapper = modelMapper;
+    }
+
+    @Override
+    public ModulResponse Add(AddModulRequest request) {
+        String id = modulRepository.sp_Module_Add(request.getName(), request.getDauer());
+        Modul modul = modulRepository.sp_Module_GetById(id);
+        return modelMapper.map(modul, ModulResponse.class);
+    }
+
+    @Override
+    public void Delete(DeleteModulRequest request, boolean permanent) {
+        if (permanent) {
+            modulRepository.sp_Module_DeletePermanent(request.getId());
+            return;
+        }
+        modulRepository.sp_Module_Delete(request.getId());
+    }
+
+    @Override
+    public ModulResponse Edit(EditModulRequest request) {
+        modulRepository.sp_Module_Update(request.getId(), request.getName(), request.getDauer());
+        Modul modul = modulRepository.sp_Modul_GetById(request.getId());
+        return modelMapper.map(modul, ModulResponse.class);
+    }
+
+    @Override
+    public Iterable<ModulResponse> Get(boolean deleted) {
+        Iterable<Modul> modul;
+
+        if (deleted) {
+            modul = modulRepository.sp_Module_GetDeleted();
+        } else {
+            modul = modulRepository.sp_Module_Get();
+        }
+
+        return modelMapper.map(modul, new TypeToken<Iterable<ModulResponse>>() {
+        }.getType());
+    }
+
+    @Override
+    public ModulResponse GetById(GetModulRequest request) {
+        Modul modul = modulRepository.sp_Module_GetById(request.getId());
+        return modelMapper.map(modul, ModulResponse.class);
+    }
+
+    @Override
+    public Iterable<ModulResponse> Search(SearchModulRequest request) {
+        Iterable<Modul> modul = modulRepository.sp_Module_Search(request.getName());
+        return modelMapper.map(modul, new TypeToken<Iterable<ModulResponse>>() {
+        }.getType());
+    }
+}
