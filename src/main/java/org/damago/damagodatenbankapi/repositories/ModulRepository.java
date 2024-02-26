e9e7ac4184b078d41629ea511dfa409554b5cd82
Rojhat Erdogan <Ro.Erdogan@icloud.com>
2024-02-26 09:12:54 +0100

    Module - Check

--- src/main/java/org/damago/damagodatenbankapi/repositories/ModulRepository.java
+++ src/main/java/org/damago/damagodatenbankapi/repositories/ModulRepository.java
@@ -1,6 +1,5 @@
 package org.damago.damagodatenbankapi.repositories;
 
-import org.damago.damagodatenbankapi.entities.Klasse;
 import org.damago.damagodatenbankapi.entities.Modul;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.query.Procedure;
@@ -9,29 +8,29 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public interface ModulRepository extends JpaRepository<Modul,String > {
     @Procedure
-    String sp_Module_Add(String name, String beschreibung);
+    String sp_Modules_Add(String name, String beschreibung);
 
     @Procedure
-    void sp_Module_Delete(String id);
+    void sp_Modules_Delete(String id);
 
     @Procedure
-    void sp_Module_DeletePermanent(String id);
+    void sp_Modules_DeletePermanent(String id);
 
     @Procedure
-    Iterable<Modul> sp_Module_Get();
+    Iterable<Modul> sp_Modules_Get();
 
     @Procedure
-    Modul sp_Module_GetById(String id);
+    Modul sp_Modules_GetById(String id);
 
     @Procedure
-    Iterable<Modul> sp_Module_GetDeleted();
+    Iterable<Modul> sp_Modules_GetDeleted();
 
     @Procedure
-    Iterable<Modul> sp_Module_Search(String name);
+    Iterable<Modul> sp_Modules_Search(String name);
 
     @Procedure
     void sp_Module_Undelete(String id);
 
     @Procedure
-    void sp_Module_Update(String id, String name, String beschreibung);
+    void sp_Modules_Update(String id, String name, String beschreibung);
 }
